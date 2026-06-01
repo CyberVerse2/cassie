163c56e5b48676df07c4b611d8bd74e4cbcb45f1
Celestine <ejioforcelestine77@gmail.com>
2026-06-01 13:37:58 +0100

    Reload worker on code changes

--- src/worker.ts
+++ src/worker.ts
@@ -2,6 +2,17 @@ import "dotenv/config";
 import { runExecutionWorker } from "../packages/jobs/worker.ts";
 
 const runner = await runExecutionWorker();
+let stopping = false;
+
+async function stopWorker(signal: NodeJS.Signals) {
+  if (stopping) return;
+  stopping = true;
+  console.log(`Cassie Graphile Worker stopping after ${signal}.`);
+  await runner.stop();
+}
+
+process.once("SIGINT", () => void stopWorker("SIGINT"));
+process.once("SIGTERM", () => void stopWorker("SIGTERM"));
 
 console.log("Cassie Graphile Worker started.");
 
